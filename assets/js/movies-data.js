3860fde312260cbe9b51baf5cfd46f5226c5d735
roshannoonoottil <roshannoonoottil@gmail.com>
2023-10-15 15:42:18 +0530

    1st commit

---
+++ assets/js/movies-data.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'The Falcon and the Winter Soldier is an American television miniseries created by Malcolm Spellman.',
+        image: 'assets/img/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Loki is an American television series created by Michael Waldron for the streaming service Disney+.',
+        image: 'assets/img/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'WandaVision is an American television miniseries created by Jac Schaeffer for the streaming service Disney+.',
+        image: 'assets/img/slider 3.PNG'
+    },
+    {
+        name: 'Ahsoka',
+        des: 'Ahsoka, also known as Star Wars: Ahsoka, is an American television miniseries created and written by Dave Filoni.',
+        image: 'assets/img/ahsoka.jpeg'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Raya and the Last Dragon is a 2021 American computer-animated fantasy action-adventure film produced by Walt Disney. ',
+        image: 'assets/img/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Luca is a 2021 American computer-animated coming-of-age fantasy film produced by Walt Disney Pictures and Pixar Animation Studios.',
+        image: 'assets/img/slider 5.PNG'
+    }
+]
